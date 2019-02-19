0f8ff2c79efbbfaa8c3b3abebbff28ed18156554
silentscribe <seelive.i@gmail.com>
2019-02-19 21:08:34 +0000

    linting and updated models

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,10 +3,6 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
-class Store 
-end
-
 burnaby = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 richmond = Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 gastown = Store.create(name: 'Gastown', annual_revenue: 300000, mens_apparel: true, womens_apparel: false)
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,15 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Pete", last_name: "Stewardson", hourly_rate: 60)
+@store1.employees.create(first_name: "Kyle", last_name: "Robson", hourly_rate: 50)
+@store1.employees.create(first_name: "Sam", last_name: "Davey", hourly_rate: 70)
+@store1.employees.create(first_name: "Tony", last_name: "Fraser", hourly_rate: 12)
+@store1.employees.create(first_name: "Harold", last_name: "Nicolson", hourly_rate: 16)
+@store1.employees.create(first_name: "Sally", last_name: "Matlo", hourly_rate: 70)
+@store2.employees.create(first_name: "Tonya", last_name: "Johnston", hourly_rate: 18)
+@store2.employees.create(first_name: "Nic", last_name: "Hill", hourly_rate: 22)
+@store2.employees.create(first_name: "Jill", last_name: "Sheron", hourly_rate: 29)
+@store2.employees.create(first_name: "Hanna", last_name: "Good", hourly_rate: 33)
+@store2.employees.create(first_name: "Peter", last_name: "Misha", hourly_rate: 37)
+
